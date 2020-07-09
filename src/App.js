fa651daaaf1b10994eaec5f6e4eb7701eeda5fbd
pranjals149 <42092291+pranjals149@users.noreply.github.com>
2020-07-09 22:34:04 +0530

    display-apod

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import DateInput from "./components/DateInput";
+import Photo from "./components/Photo.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    date: "",
+    photo: ""
+  };
+
+  changeDate = e => {
+    e.preventDefault();
+    let dateFromInput = e.target[0].value;
+    this.setState({ date: dateFromInput });
+    this.getPhoto(dateFromInput);
+  };
+
+  getPhoto = date => {
+    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=EbInQmEzXObNwCxF9gY8llrfdKuoLkH2ZWwOjS2G`)
+      .then(response => response.json())
+      .then(photoData => this.setState({ photo: photoData }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>NASA's Astronomy Picture of the Day</h1>
+        <DateInput changeDate={this.changeDate} />
+        <Photo photo={this.state.photo} />
+      </div>
+    );
+  }
 }
 
 export default App;
